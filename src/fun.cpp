1f7f88c2aa33d42170e721fbec756394cfa4da1e
psmnov <130275227+psmnov@users.noreply.github.com>
2026-03-29 10:07:54 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -3,7 +3,7 @@
 
 int64_t power(int64_t x, uint16_t n) {
     uint64_t res = 1;
-    for(uint16_t i = 0; i<= n; i++){
+    for (uint16_t i = 0; i < n; i++) {
         res *= x;
     }
     return res;
